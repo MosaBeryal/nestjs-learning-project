713bdaa52c7951d3539d2e8d260d1f5cee566948
mosa beryal <beryalmosa@gmail.com>
2024-08-20 00:59:39 +0500

    application configuration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // Get users
+  @Get()
+  findAll() {
+    return ['User 1', 'User 2', 'User 3'];
+  }
+  //Get user by id
+  @Get(':id')
+  findOneById(@Param('id') id: string) {
+    return `User with ID: ${id}`;
+  }
+}
